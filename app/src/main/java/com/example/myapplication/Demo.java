24b80476e8ae95e0c68204f0e17e4d758970ba15
Palak Maradiya <palakmaradiya1594@gmail.com>
2023-04-14 17:55:54 +0530

    mainactivity 4 added

---
+++ app/src/main/java/com/example/myapplication/Demo.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import android.util.Log;
+
+public class Demo {
+
+    public int a=10;
+
+
+    public void Printdata()
+    {
+        Log.e("TAG", "Button is click "+a);
+    }
+
+
+}
